c9a520d20bc8bd8446e4019dd578b2dd1d6fb5b6
Shatrughan <shatru@Shatrughans-iMac.local>
2021-09-13 15:02:00 +0530

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Magexweb Advancecategory
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category  Magexweb
+ * @package   Magexweb_Advancecategory
+ * @copyright Copyright (c) 2021 Magexweb Advancecategory (https://www.magexweb.com/)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Magexweb Team <support@magexweb.com>
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Magexweb_Core',
+    __DIR__
+);
